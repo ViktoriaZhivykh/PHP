3ab0f290cda2560dd87ff39b24644ade8eb7c174
ViktoriaZhivykh <119225213+ViktoriaZhivykh@users.noreply.github.com>
2024-12-22 01:27:24 +0300

    Create logger.php

---
+++ Task 5/logger.php
@@ -0,0 +1,36 @@
+<?php
+
+// Интерфейс реализует логирование сообщений
+interface LoggerInterface {
+    public function log(string $message): void;
+}
+
+// Класс реалтзует LoggerInterface
+class DatabaseLogger implements LoggerInterface {
+    protected $pdo;
+
+    // Соединение с базой данных MySQL
+    public function __construct() {
+        $user = 'root';
+        $password = 'mypass';
+        $db = 'php_study';
+        $host = '127.0.0.1';
+        $port = 3306;
+        $conn = mysqli_connect(hostname: $host, username: $user, password: $password, database: $db);
+        $this->pdo = $conn;
+    }
+
+    // Запись в таблицу базы данных
+    public function log(string $message): void {
+        $sql = "INSERT INTO logs (message, created_at) VALUES (?, NOW())";
+        $stmt = $this->pdo->prepare(query: $sql);
+        $stmt->bind_param(types: "s", var: $message);
+        $stmt->execute();
+        $stmt->close();
+    }
+}
+
+// Создание экземпляра и вызов метода log
+$logger = new DatabaseLogger();
+$logger->log(message: "Test log");
+?>
